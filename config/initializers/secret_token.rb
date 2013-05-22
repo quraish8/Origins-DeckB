bd87f03fff54565164d3818b3d38e0ea7950c4cb
Assad Quraishi <assad.quraishi@mail.utoronto.ca>
2013-05-22 11:16:26 -0400

    Actual inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OriginsDeckB::Application.config.secret_token = '0ab57707dc8047b55d7974404e0e0a790cd4d369de945616a7eaa6ed4bb6daa2361c8fb194cbb42237e092532fa6cc18e6082548ae11953be76f948db42fa5cc'
